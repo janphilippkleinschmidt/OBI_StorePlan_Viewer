dd9e9bc5db4b1a8f85133b44dd92bf6201eb12a1
janphilippkleinschmidt <jan.philipp.kleinschmidt@obi.de>
2025-02-02 16:53:03 +0100

    fixes and improvements
    -now not only EG and OG are supported but also UG
    -Plans opened in a new tab should now be zoomed in/out correctly
    -fixed an issue that cropped some larger plans

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ document.getElementById('marketForm').addEventListener('submit', async function(
     const errorDiv = document.getElementById('error-message');
     const downloadButton = document.getElementById('downloadButton');
     const submitButton = document.querySelector('#marketForm button[type="submit"]');
+
+    const baseURL = 'https://api.live.app.obi.de/v1/stores/';
     
     // Reset previous states
     errorDiv.style.display = 'none';
@@ -17,7 +19,7 @@ document.getElementById('marketForm').addEventListener('submit', async function(
 
     try {
         const proxyUrl = 'https://proxy.cors.sh/';
-        const apiUrl = `${proxyUrl}https://api.live.app.obi.de/v1/stores/${marktNumber}?country=${email.slice(-2)}`;
+        const apiUrl = `${proxyUrl}${baseURL}${marktNumber}?country=${email.slice(-2)}`;
         
         const options = {
             method: 'GET',
@@ -39,13 +41,15 @@ document.getElementById('marketForm').addEventListener('submit', async function(
         const doc = parser.parseFromString(svgText, 'text/xml');
         const section1 = doc.querySelector('g[id="1"]');
         const section0 = doc.querySelector('g[id="0"]');
+        const section1Var = doc.querySelector('g[id="-1"]');
+
 
         // Create separate SVGs for each section
         const createSectionSVG = (sectionId, sectionContent) => {
             return `
                 <?xml version="1.0" encoding="utf-8"?>
-                <svg width="3000px" height="2000px" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                viewBox="0 0 3000 2000" enable-background="new 0 0 3000 2000" xml:space="preserve">
+                <svg width="100%" height="auto" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 3000 3000" preserveAspectRatio="xMidYMid meet" enable-background="new 0 0 3000 2000" xml:space="preserve">
                     ${sectionContent}
                 </svg>
             `;
@@ -60,7 +64,7 @@ document.getElementById('marketForm').addEventListener('submit', async function(
         };
 
         // if store has multiple storeys 
-        if (section1 && section0) {
+        if ((section1 || section1Var) && section0) {
             const existingViewButtons = document.querySelectorAll('.view-buttons-container');
             existingViewButtons.forEach(button => button.remove());
             // Remove existing display:none from HTML
@@ -71,7 +75,7 @@ document.getElementById('marketForm').addEventListener('submit', async function(
             document.querySelector('.download-options').style.display = 'block';
             document.getElementById('downloadButton').style.display = 'block';
             // Get complete content for each section including all children
-            const section1Content = getCompleteSectionContent(section1);
+            const section1Content = section1 ? getCompleteSectionContent(section1) : getCompleteSectionContent(section1Var);
             const section0Content = getCompleteSectionContent(section0);
 
             // Open in separate tabs
@@ -88,7 +92,7 @@ document.getElementById('marketForm').addEventListener('submit', async function(
             const viewButtonsContainer = document.createElement('div');
             viewButtonsContainer.className = 'view-buttons-container';
             viewButtonsContainer.innerHTML = `
-                <button id="viewSection1" class="view-btn">Obergeschoss anzeigen</button>
+                <button id="viewSection1" class="view-btn">Unter-/Obergeschoss anzeigen</button>
                 <button id="viewSection0" class="view-btn">Erdgeschoss anzeigen</button>
             `;
             
