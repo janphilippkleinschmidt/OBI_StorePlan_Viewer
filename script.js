1879e47402baebf9394d893ab82bd6c5f752c124
janphilippkleinschmidt <jan.philipp.kleinschmidt@obi.de>
2025-03-05 08:36:21 +0100

    cleaned up "download button" code

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.getElementById('marketForm').addEventListener('submit', async function(
 
     const marktNumber = document.getElementById('marktNumber').value; //userinput "Marktnummer"
     const email = document.getElementById('email').value; //userinput "E-Mail Adresse"
-    // const downloadButton = document.getElementById('downloadButton');
     const submitButton = document.querySelector('#marketForm button[type="submit"]');
     const submitButtonElement = document.getElementById('submitbutton');
     const FormButton = document.querySelector('.report-changes-container .report-changes-link');
+    const reportChangesContainer = document.querySelector('.report-changes-container');
 
     const PROXY_API_KEY = "{{ PROXY_API_KEY }}";
     const OBI_URL = "{{ OBI_API }}";
@@ -14,20 +14,10 @@ document.getElementById('marketForm').addEventListener('submit', async function(
     const proxyUrl = 'https://proxy.cors.sh/';
 
     var storename = '';
-
-    //Custom Forms URL
-    var FormQ1 = marktNumber;
-    var FormQ2 = email;
-    var FormQ3 = '';
-    var FormQ4 = '';
-    var FormBaseURL = `https://docs.google.com/forms/d/e/1FAIpQLSfDlNxdmDdmLCrGu71CuLTMxYXZ7hoRSpO82xuIv5XqEyXOlw/viewform?usp=pp_url&entry.1016517630=${FormQ1}&entry.1540606585=${FormQ2}&entry.1866007651=${FormQ3}&entry.728342376=${FormQ4}`;
-
-    FormButton.href = FormBaseURL;
-
     
     function updateStatus(message, type = 'loading') {
         const statusDiv = document.querySelector('.status-content');
-        statusDiv.innerHTML = ''; // Lösche vorherigen Status
+        statusDiv.innerHTML = ''; // Delete previous status
         
         const statusStep = document.createElement('div');
         statusStep.className = `status-step status-${type}`;
@@ -56,9 +46,21 @@ document.getElementById('marketForm').addEventListener('submit', async function(
     }
 
     updateStatus('Starte Marktplan-Anfrage...');
+
+    function setFormURL() { //Custom/ dynamic Google Forms URL
+        var FormQ1 = marktNumber; //markt nummer field
+        var FormQ2 = email; //email field
+        var FormQ3 = ''; // Ja / Nein selection
+        var FormQ4 = ''; //änderungsbedarf text field
+        var FormBaseURL = `https://docs.google.com/forms/d/e/1FAIpQLSfDlNxdmDdmLCrGu71CuLTMxYXZ7hoRSpO82xuIv5XqEyXOlw/viewform?usp=pp_url&entry.1016517630=${FormQ1}&entry.1540606585=${FormQ2}&entry.1866007651=${FormQ3}&entry.728342376=${FormQ4}`;
+
+        FormButton.href = FormBaseURL;
+    }
+
+    setFormURL();
+    updateStatus('Anpassung der Google Forms URL');
     
-    //downloadButton.style.display = 'none'; // Reset downloadbutton state
-    const existingViewButtons = document.querySelectorAll('.view-buttons-container');
+    const existingViewButtons = document.querySelectorAll('.view-buttons-container'); //remove the "Marktplan anzeigen" buttons
     existingViewButtons.forEach(button => button.remove());
 
     try {
@@ -76,7 +78,7 @@ document.getElementById('marketForm').addEventListener('submit', async function(
             },
         };
         
-        updateStatus('Marktplan wird angefordert...'); //Warte auf API-Antwort..
+        updateStatus('Marktplan wird angefordert...'); //Waiting for API response...
         const response = await fetch(apiUrl, options);
         
         if (!response.ok) {
@@ -87,7 +89,7 @@ document.getElementById('marketForm').addEventListener('submit', async function(
             }
         }
         
-        updateStatus('Marktplan erhalten...'); //API-Antwort erhalten...
+        updateStatus('Marktplan erhalten...'); //Received API response
         const svgText = await response.text();
         console.log('Received SVG:', svgText.slice(0, 100));
         
@@ -102,29 +104,28 @@ document.getElementById('marketForm').addEventListener('submit', async function(
 
         
         updateStatus('Identifiziere Marktplan-Ebenen...');
-        const section1 = doc.querySelector('g[id="1"]');
-        const section0 = doc.querySelector('g[id="0"]');
-        const section1Var = doc.querySelector('g[id="-1"]');
+        const storey_EG = doc.querySelector('g[id="0"]'); //EG
+        const storey_OG = doc.querySelector('g[id="1"]'); //OG
+        const storey_UG = doc.querySelector('g[id="-1"]'); //UG
         
         updateStatus('Erstelle separate SVG-Dokumente...');
-        const createSectionSVG = (sectionId, sectionContent) => {
+        const createstorey_SVG = (sectionId, storey_Content) => {
             return `
                 <?xml version="1.0" encoding="utf-8"?>
                 <svg width="2000px" height="2000px" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                 viewBox="0 0 3000 3000" preserveAspectRatio="xMidYMid meet" enable-background="new 0 0 3000 2000" xml:space="preserve">
-                ${sectionContent}
+                ${storey_Content}
                 </svg>
             `;
         };
         
-        const getCompleteSectionContent = (section) => {
+        const getCompletestorey_Content = (section) => {
             const serializer = new XMLSerializer();
-            const sectionContent = serializer.serializeToString(section);
-            return sectionContent;
+            const storey_Content = serializer.serializeToString(section);
+            return storey_Content;
         };
 
-        // Titel extrahieren und protokollieren
-        storename = await extractTitleFromHtml(svgText);
+        storename = await extractTitleFromHtml(svgText); //save title of html to display in the "success" message
         console.log('Marktname:', storename);
 
         async function extractTitleFromHtml(html) {
@@ -142,37 +143,31 @@ document.getElementById('marketForm').addEventListener('submit', async function(
         updateStatus('Bereite Markplan vor...');
         const existingViewButtons = document.querySelectorAll('.view-buttons-container');
         existingViewButtons.forEach(button => button.remove());
-
-        // document.querySelector('.download-options').removeAttribute('style');
-        // document.getElementById('downloadButton').removeAttribute('style');
-        // document.querySelector('.download-options').style.display = 'block';
-        // document.getElementById('downloadButton').style.display = 'block';
         
-        
-        if ((section1 || section1Var) && section0) { // Mehrere Etagen gefunden
+        if ((storey_OG || storey_UG) && storey_EG) { // found multiple storeys
             
-            const section1Content = section1 ? getCompleteSectionContent(section1) : getCompleteSectionContent(section1Var);
-            const section0Content = getCompleteSectionContent(section0);
+            const storey_OG_UG_Content = storey_OG ? getCompletestorey_Content(storey_OG) : getCompletestorey_Content(storey_UG);
+            const storey_EG_Content = getCompletestorey_Content(storey_EG);
             
             updateStatus('Erstelle Ansicht-Links...');
-            const section1SVG = createSectionSVG('1', section1Content);
-            const section0SVG = createSectionSVG('0', section0Content);
+            const storey_OG_UG_SVG = createstorey_SVG('1', storey_OG_UG_Content);
+            const storey_EG_SVG = createstorey_SVG('0', storey_EG_Content);
             
             updateStatus('Erstelle Download-Links...');
-            const blob1 = new Blob([section1SVG], { type: 'text/html' });
+            const blob1 = new Blob([storey_OG_UG_SVG], { type: 'text/html' });
             const url1 = URL.createObjectURL(blob1);
-            const blob0 = new Blob([section0SVG], { type: 'text/html' });
+            const blob0 = new Blob([storey_EG_SVG], { type: 'text/html' });
             const url0 = URL.createObjectURL(blob0);
             
             updateStatus('Erstelle "Anzeigen"-Schaltfläche...');
             const viewButtonsContainer = document.createElement('div');
             viewButtonsContainer.className = 'view-buttons-container';
-            if (section1) {
+            if (storey_OG) {
                 viewButtonsContainer.innerHTML = `
                     <button id="viewSection1" class="view-btn">Obergeschoss anzeigen</button>
                     <button id="viewSection0" class="view-btn">Erdgeschoss anzeigen</button>
                 `;
-            } else if (section1Var) {
+            } else if (storey_UG) {
                 viewButtonsContainer.innerHTML = `
                     <button id="viewSection1" class="view-btn">Untergeschoss anzeigen</button>
                     <button id="viewSection0" class="view-btn">Erdgeschoss anzeigen</button>
@@ -184,83 +179,52 @@ document.getElementById('marketForm').addEventListener('submit', async function(
             updateStatus('Füge Event-Listener hinzu...');
             document.getElementById('viewSection1').addEventListener('click', () => {
                 window.open(url1, '_blank');
+
+                reportChangesContainer.style.display = 'block'; // Show the "Report Changes" button
             });
             
             document.getElementById('viewSection0').addEventListener('click', () => {
                 window.open(url0, '_blank');
+
+                reportChangesContainer.style.display = 'block'; // Show the "Report Changes" button
             });
-            
-            // updateStatus('Konfiguriere Download-Funktion...');
-            // downloadButton.onclick = () => {
-            //     updateStatus('Starte Download-Vorgang...');
-            //     const downloadSection = (sectionId, sectionContent) => {
-            //         const sectionSVG = createSectionSVG(sectionId, sectionContent);
-            //         const blob = new Blob([sectionSVG], { type: 'text/html' });
-            //         const url = URL.createObjectURL(blob);
-            //         const a = document.createElement('a');
-            //         a.href = url;
-            //         a.download = `Marktplan-${marktNumber}-Etage-${sectionId}.html`;
-            //         a.click();
-            //         URL.revokeObjectURL(url);
-            //     };
-                
-            //     downloadSection('1', section1Content);
-            //     downloadSection('0', section0Content);
-            //     updateStatus('Download abgeschlossen: ', 'success');
-            // };
             
             updateStatus('Fertig! Markplan mit mehreren Stockwerken verfügbar: ', 'success');
             
-        } else { // Eine Etage gefunden
+        } else { // found one storey
 
-            const sectionContent = getCompleteSectionContent(doc);
+            const storey_Content = getCompletestorey_Content(doc);
 
-            const sectionSVG = createSectionSVG('0',sectionContent);
+            const storey_SVG = createstorey_SVG('0',storey_Content);
             
-            const blob = new Blob([sectionSVG], { type: 'text/html' });
+            const blob = new Blob([storey_SVG], { type: 'text/html' });
             const url = URL.createObjectURL(blob);
             
             updateStatus('Erstelle "Anzeigen"-Schaltfläche...');
             const viewButtonsContainer = document.createElement('div');
             viewButtonsContainer.className = 'view-buttons-container';
             viewButtonsContainer.innerHTML = `
-                <button id="viewPlan" class="view-btn">Marktplan anzeigen</button>
+                <button id="viewSection1" class="view-btn">Marktplan anzeigen</button>
             `;
             
             document.querySelector('.button-container').append(viewButtonsContainer);
             
             updateStatus('Füge Event-Listener hinzu...');
-            document.getElementById('viewPlan').addEventListener('click', () => {
+            document.getElementById('viewSection1').addEventListener('click', () => {
                 window.open(url, '_blank');
+
+                reportChangesContainer.style.display = 'block'; // Show the "Report Changes" button
             });
-            
-            // updateStatus('Konfiguriere Download-Funktion...');
-            // downloadButton.onclick = () => {
-            //     updateStatus('Starte Download-Vorgang...');
-            //     const downloadFile = () => {
-            //         const blob = new Blob([sectionSVG], { type: 'text/html' });
-            //         const url = URL.createObjectURL(blob);
-            //         const a = document.createElement('a');
-            //         a.href = url;
-            //         a.download = `Marktplan-${marktNumber}.html`;
-            //         a.click();
-            //         URL.revokeObjectURL(url);
-            //     };
-                
-            //     downloadFile();
-            //     updateStatus('Download abgeschlossen: ', 'success');
-            // };
             
             updateStatus('Fertig! Markplan verfügbar: ', 'success');
         }
         
     } catch (error) {
-        if (error.message.includes("404")) {
+        if (error.message.includes("404")) { //404 indicates that the combination of the storen umber and the country code dont match
             updateStatus(`Fehler aufgetreten: ${error.message}.`, 'error');
         } else {
             updateStatus(`Fehler aufgetreten: ${error.message}. Bitte versuche es in ein paar Minuten erneut.`, 'error');
         }
-        
         
         console.error('Request failed:', {
             status: error.status,
@@ -268,7 +232,7 @@ document.getElementById('marketForm').addEventListener('submit', async function(
             url: error.url
         });
     } finally {
-        submitButtonElement.disabled = false;
+        submitButtonElement.disabled = false; //reactivate submit button
         submitButton.classList.remove('disabled-button');
     }
 });
